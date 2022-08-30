b768a1ac3d855f1ed46fc1e3ca13f960eb28fab2
Jim Pick <jim@jimpick.com>
2022-08-29 22:17:31 -0700

    Try to rebuild and push latest tag

--- run-docker.sh
+++ run-docker.sh
@@ -4,3 +4,4 @@ docker rm -f pq-sql-publisher
 docker run -it --name pq-sql-publisher pq-sql-publisher
 #docker run -it --cap-add=SYS_ADMIN --entrypoint /bin/bash --cap-add=SYS_ADMIN --name pq-sql-publisher pq-sql-publisher
 #docker run -it -u 0 --cap-add=SYS_ADMIN --entrypoint /bin/bash --cap-add=SYS_ADMIN --name pq-sql-publisher pq-sql-publisher
+#docker run -it --name pq-sql-publisher ghcr.io/provider-quest/provider-quest-sql-publisher@sha256:6869aa536cb31cbb59879559278094e766fce73316f817db6911dc68185687e4
