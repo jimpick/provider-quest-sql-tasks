e4930bd81cc84a95f82e5b4dc18dcb3990419ae5
Jim Pick <jim@jimpick.com>
2022-09-02 18:53:30 -0700

    Create tables and load CSV data

---
+++ create-tables.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS public.provider_power_daily;
+CREATE TABLE public.provider_power_daily (
+    provider character varying(20) NOT NULL,
+    date date,
+    "avg(rawBytePower)" double precision,
+    "avg(qualityAdjPower)" double precision
+);
+ALTER TABLE public.provider_power_daily OWNER TO postgres;
+CREATE INDEX provider_power_daily_date_idx ON public.provider_power_daily USING btree (date);
+
+DROP TABLE IF EXISTS public.synthetic_regions;
+CREATE TABLE public.synthetic_regions (
+    provider character varying(255),
+    region character varying(255),
+    "numRegions" integer,
+    delegate character varying(255)
+);
+ALTER TABLE public.synthetic_regions OWNER TO postgres;
+CREATE INDEX synthetic_regions_delegate_idx ON public.synthetic_regions USING btree (delegate);
+CREATE INDEX synthetic_regions_provider_idx ON public.synthetic_regions USING btree (provider);
+CREATE INDEX synthetic_regions_region_idx ON public.synthetic_regions USING btree (region);
+
+DROP TABLE IF EXISTS public.deals_daily;
+CREATE TABLE public.deals_daily (
+    date date NOT NULL,
+    provider character varying NOT NULL,
+    "verifiedDeal" boolean NOT NULL,
+    count numeric NOT NULL,
+    "sum(pieceSizeDouble)" numeric NOT NULL,
+    "avg(pieceSizeDouble)" numeric NOT NULL,
+    "min(pieceSizeDouble)" numeric NOT NULL,
+    "max(pieceSizeDouble)" numeric NOT NULL,
+    "avg(storagePricePerEpochDouble)" numeric NOT NULL,
+    "min(storagePricePerEpochDouble)" numeric NOT NULL,
+    "max(storagePricePerEpochDouble)" numeric NOT NULL,
+    "approx_count_distinct(label)" numeric NOT NULL,
+    "sum(lifetimeValue)" numeric NOT NULL,
+    "avg(lifetimeValue)" numeric NOT NULL,
+    "min(lifetimeValue)" numeric NOT NULL,
+    "max(lifetimeValue)" numeric NOT NULL,
+    "approx_count_distinct(provider)" boolean NOT NULL,
+    "approx_count_distinct(client)" numeric NOT NULL,
+    "approx_count_distinct(clientProvider)" numeric NOT NULL
+);
+ALTER TABLE public.deals_daily OWNER TO postgres;
+
+
--- load-data-and-process.sh
+++ load-data-and-process.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+set -e
+set +x
+
 wget https://sp-power-daily.feeds.provider.quest/provider-power-daily.csv
 wget https://geoip.feeds.provider.quest/synthetic-regions-latest.json
 wget https://provider-quest.s3.us-west-2.amazonaws.com/csp-regions-monthly.csv
@@ -9,12 +12,13 @@ cat synthetic-regions-latest.json | jq .regions  | jq -r '(.[0] | keys_unsorted)
 
 export PGPASSWD=insecure
 
-exit
+cat create-tables.sql | psql --host localhost -U postgres -d postgres -p 5432 -f -
 
 cat <<EOF | psql --host localhost -U postgres -d postgres -p 5432 -f -
 
-DELETE FROM provider_power_daily;
-
 \copy provider_power_daily FROM 'provider-power-daily.csv' DELIMITER ',' CSV HEADER;
+\copy deals_daily FROM 'deals-daily.csv' DELIMITER ',' CSV HEADER;
+\copy synthetic_regions FROM 'synthetic-regions-latest.csv' DELIMITER ',' CSV HEADER;
+\copy csp_regions_monthly FROM 'csp-regions-monthly.csv' DELIMITER ',' CSV HEADER;
 
 EOF
